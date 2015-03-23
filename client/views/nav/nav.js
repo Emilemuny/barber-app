6183b58b5a7d07a4dbaa7a057628c1adbdf031e2
emilemuny <emilemuny007@gmail.com>
2015-03-23 04:15:51 -0700

    improved the navbar with ng-if to show or not -working

---
+++ client/views/nav/nav.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('barber-book')
+ .controller('NavCtrl', ['$rootScope', '$scope', '$state', '$window', function($rootScope, $scope, $state, $window){
+   $scope.logout = function(){
+     delete $rootScope.business;
+     delete $rootScope.user;
+     $window.localStorage.clear();
+     $state.go('authenticate');
+   };
+
+ }]);
