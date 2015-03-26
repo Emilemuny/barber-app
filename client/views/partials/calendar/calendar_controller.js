7a03bf19124160ad3d32958541a7b3d4a6250771
emilemuny <emilemuny007@gmail.com>
2015-03-26 03:10:16 -0700

    added datetimepicker css and angular scope -working HALF

--- client/views/partials/calendar/calendar_controller.js
+++ client/views/partials/calendar/calendar_controller.js
@@ -3,15 +3,29 @@
 angular.module('barber-book')
  .controller('CalCtr', ['$scope', function($scope){
 
+  $scope.sendAppt = function(apptData) {
+    console.log('apptData', $scope.sendAppt );
+    console.log('appDATA', apptData);
+  };
 
-   $(function () {
-     $('#datetimepicker5').datetimepicker({
-       defaultDate: '11/1/2013',
-       disabledDates: [
-         moment('12/25/2013'),
-         new Date(2013, 11 - 1, 21),
-         '11/22/2013 00:53'
-       ]
-     });
-   });
+
+  $(function () {
+    $('#datetimepicker5').datetimepicker({
+      //inline: true,
+      //sideBySide: true,
+      defaultDate: '11/1/2013',
+      disabledDates: [
+        moment('12/25/2013'),
+        new Date(2013, 11 - 1, 21),
+        '11/22/2013 00:53'
+      ]
+    });
+  });
+
+  $('#exampleModal').on('hide.bs.modal', function(e){
+    if(isBlocked) return e.preventDefault();
+  });
+
+
+
  }]);
